415a4ee071f03d24a78355784561190d585d80ba
Kat-Stack <doughkathryn@gmail.com>
2021-11-22 17:28:27 -0500

    final version before I probably don't post many more public ones. I added an interaction log for a self-percolating discussion in baby bot(now Gaulle)'s corpus. We will see what happens

--- main.py
+++ main.py
@@ -5,36 +5,122 @@ flag = True
 
 
 my_secret = os.environ['TOKEN']
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='`')
+initFlag = False
+talkOrListen = {}
+autogenInt = 1
+autogenCounter = 1
 
 
 
 
 @bot.event
 async def on_ready():
-  print('We have logged in as {0.user}'.format(bot) + "\nPlease use $init with any applicable corpi separated with a space or leave blank for defaults\nRegardless, $init or I won't work")
-    
+  print('We have logged in as {0.user}'.format(bot) + "\nPlease use $init with any applicable corpi separated with a space or leave blank for defaults\nRegardless, `init or I won't work")
+
+
+@bot.command()
+async def autogen(ctx, autogenAmount):
+    global autogen
+    global autogenCounter
+    global autogenInt
+    autogen = autogenAmount
+    autogenCounter = 1
+    await ctx.channel.send("I will now message every {} messages. AutoGeneration Counter is {}".format(autogenAmount,autogenCounter))
+
+
+@bot.command()
+async def newChann(ctx, *, name=None):
+    guild = ctx.message.guild
+    nameIsNoneFlag = True #changes to false if correct
+    if name == None:
+        await ctx.send('Sorry, but you have to insert a name. Try again, but do it like this: `create [channel name]')
+        nameIsNoneFlag = False
+    if nameIsNoneFlag:
+        allChannels = {}
+        for chann in guild.text_channels:
+            allChannels[chann] = []
+        if name in allChannels:
+            await ctx.send('Sorry, but this channel already exists. Try again, but do it with a different name: `create [channel name]')
+            return
+        else:
+            await guild.create_text_channel(name)
+            await ctx.send(f"Created a channel named {name}. Message 'talk to begin there!")
+            return
+
+
+
+
+@bot.command()
+async def talkAll(ctx):
+    guild = ctx.channel.guild
+    for chann in guild.text_channels:
+        try:
+            if chann not in talkOrListen:
+                talkOrListen[chann] = True
+                await chann.send("I am SUPER CHATTY BABY BOT GAULLE MWAHAHA (Active in all channels on this server)")
+            else:
+                talkOrListen[chann] = True
+                await chann.send("Someone told me to talk.. idk I'm just following directions")
+        except:
+            pass
+
+
+@bot.command()
+async def talk(ctx):
+    if ctx.channel not in talkOrListen:
+        talkOrListen[ctx.channel] = True
+        await ctx.channel.send("I am feeling chatty")
+    else:
+        if talkOrListen[ctx.channel]:
+            talkOrListen[ctx.channel] = False
+            await ctx.channel.send("I am listening here")
+        else:
+            talkOrListen[ctx.channel] = True
+            await ctx.channel.send("I'm gonna talk mmkay?")
+
+
+
 @bot.command()
 async def init(ctx, *corpi):
-  if len(corpi) == 0:
-    tm.corpusInit()
-  else:
-    tm.corpusInit(corpi)
-  await ctx.send("I have initialized the corpus with {} arguments".format(corpi))
-  global flag 
-  flag = False
+    global initFlag
+    if not initFlag:
+        if len(corpi) == 0:
+            tm.corpusInit()
+        else:
+            tm.corpusInit(corpi)
+        await ctx.send("I have initialized the corpus with {} arguments".format(corpi))
+        global flag
+        flag = False
+        initFlag = True
+
 
 @bot.event
 async def on_message(message):
-    await bot.process_commands(message)
-    if message.content.startswith("$init") and not flag:
-      pass
+    global autogenCounter
+    global autogenInt
+    if await bot.process_commands(message):
+        pass
     else:
-     if message.author == bot.user:
-          return
-     else:
-        response = message.content
-        await message.reply(tm.getResponse(response))
+        try:
+            if talkOrListen[message.channel]:
+                if int(autogenCounter) % int(autogenInt) == 0:
+                    if message.author == bot.user:
+                        if message.channel.name == 'gaulle':
+                            await message.reply(tm.getResponse(message.content))
+                            return
+                        else:
+                            return
+                    if not initFlag:
+                        await message.reply("Please `init me to begin.")
+                    else:
+                        response = message.content
+                        await message.reply(tm.getResponse(response))
+                autogenCounter += 1
+        except:
+            tm.processMessage(message.content)
+        finally:
+            pass
 
 
 
--- textManager.py
+++ textManager.py
@@ -1,9 +1,10 @@
 import random
 import nodesr as n
-from replit import db
+import re
 
-CORPUSBODY = ("empty", "sample")
-#"corpus.txt", "avatar.txt", "avatarScenes.txt", "interactions.txt", ("empty", "interactions.txt", "voaEnglish", "avatar", "got")
+corpusBodySource = "corpusBodySource"
+CORPUSBODY = []
+# ("empty", "interactions.txt", "voaEnglish", "avatar", "got", "redditCorpus") ("empty", "sample")
 
 corpus = """"""
 wordTagDict = {}
@@ -11,6 +12,7 @@ tagCorpusDict = {}
 ngram = {}
 corpusDict = {}
 wordLinkedListDict = {}
+db = {}
 
 
 def getCorpus():
@@ -18,71 +20,106 @@ def getCorpus():
 
 
 def populateCorpus(corpusBody=CORPUSBODY):
-  for item in corpusBody:
-    corpusList = []
-    with open ("newCorpus/" + item) as file:
-      corpusList += file.readlines()
-    for items in corpusList:
-      wordList = items.split(" ")
-      for i in range(0, len(wordList)):
-        if db.get(wordList[i-1]):
-          db[wordList[i-1]] = db[wordList[i-1]].append(wordList[i])
-        else:
-          thisList = []
-          thisList.append(wordList[i])
-          db[wordList[i-1]] = thisList
-      
+    global corpus
+    global ngram
+    global CORPUSBODY
+    corpusBody = CORPUSBODY
+    with open(corpusBodySource) as file:
+        for item in file.readlines():
+            corpusBody.append(item.strip("\n"))
+    for item in corpusBody:
+        corpusList = []
+        with open("newCorpus/" + item, encoding="utf-8") as file:
+            try:
+                corpusList += file.readlines()
+            except UnicodeDecodeError:
+                pass
+        for items in corpusList:
+            corpus += items
+    corpus = corpus.replace('\n', '.')
+    for sentence in re.split('[!.?,\t\n]', corpus):
+        for i in range(1, len(sentence.split(' '))):
+            word_pair = (sentence.split(' ')[i - 2], sentence.split(' ')[i - 1])
+            if '' in word_pair:
+                continue
+            if word_pair not in ngram:
+                ngram[word_pair] = []
+            ngram[word_pair].append(sentence.split(' ')[i])
 
-def getResponse(message):
-  out = ""
-  processedMessage = processMessage(message)
-  if len(processedMessage) <= 2:
-    counter = 2
-  elif len(processedMessage) > 2 and len(processedMessage) <= 12:
-    counter = int(len(processedMessage) / 2)
-  elif len(processedMessage) > 12:
-    counter = 12
-  usedWords = []
-  for i in range(random.randint(1,counter)):
-    word = random.choice(processedMessage)
-    if word in usedWords:
-      pass
+
+def getResponse(message, interactionsFile="newCorpus/interactions.txt"):
+    global ngram
+    try:
+        tempDict = grabATempDict(message)
+    except UnicodeDecodeError:
+        tempDict = ngram
+    if bool(tempDict):
+        word_pair = random.choice(list(tempDict.keys()))
     else:
-      if db.get(word) is None:
-        out += "I do now know what {} means".format(word)
-      else:
-        out += random.choice(db[word]) + " "
-      usedWords.append(word)
-  return out
+        word_pair = random.choice(list(ngram.keys()))
+    out = word_pair[0] + ' ' + word_pair[1] + ' '
 
-def processMessage(message):
-  splitMessage = message.split(" ")
-  if len(splitMessage) > 1:
-    sendToDB(message)
-    thisList = message.split(" ")
-  else:
-    thisList = [message]
-  return thisList
+    while True:
+        if word_pair not in ngram.keys() or word_pair[1] == ngram[word_pair] or word_pair[0] == ngram[word_pair]:
+            break
+        if len(out) > 1000:
+            break
+        third = random.choice(list(ngram[word_pair]))
+        out += third + ' '
+        word_pair = (word_pair[1], third)
 
-#pass in a sentence, saves to DB as sequences
+    write = "INITIAL MESSAGE: " + message + "MY RESPONSE: " + out + "\n"
+    f = open(interactionsFile, "a", encoding="utf-8")
+    processMessage(message)
+    f.write(write)
+    processMessage(out)
+    f.close()
+    return out
+
+def grabATempDict(message):
+    tempDict = {}
+    if len(message.split(' ')) > 1:
+
+        for i in range(1, len(message.split(' '))):
+            word_pair = (message.split(' ')[i - 2], message.split(' ')[i-1])
+            if '' in word_pair:
+                continue
+            elif (word_pair) not in tempDict:
+                tempDict[word_pair] = []
+            tempDict[word_pair].append(message.split(' ')[i])
+        return tempDict
+
+def processMessage(sentence):
+    global ngram
+    for i in range(1, len(sentence.split(' '))):
+        word_pair = (sentence.split(' ')[i - 2], sentence.split(' ')[i-1])
+        if '' in word_pair:
+            continue
+        if word_pair not in ngram:
+            ngram[word_pair] = []
+        ngram[word_pair].append(sentence.split(' ')[i])
+
+
+# pass in a sentence, saves to DB as sequences
 def sendToDB(sentence):
-  wordList = sentence.split(" ")
-  for i in range(0, len(wordList)+1):
-    if db.get(wordList[i-1]):
-      db[wordList[i-1]] = db[wordList[i-1]].append(wordList[i])
-    else:
-      thisList = []
-      thisList.append(wordList[i])
-      db[wordList[i-1]] = thisList
+    wordList = sentence.split(" ")
+    for i in range(0, len(wordList) + 1):
+        if db.get(wordList[i - 1]) and len(wordList) > i:
+            db[wordList[i - 1]] = db[wordList[i - 1]].append(wordList[i])
+        elif len(wordList) > i:
+            thisList = []
+            thisList.append(wordList[i])
+            db[wordList[i - 1]] = thisList
 
-#taking out for a bit
+
+# taking out for a bit
 def populaterCorpus(corpusBody=CORPUSBODY):
     global corpus
     global ngram
     for item in corpusBody:
         corpusDict = {}
         corpusList = []
-        with open("newCorpus/" + item) as file:
+        with open("newCorpus/" + item, encoding="utf-8") as file:
             corpusList += file.readlines()
         for items in corpusList:
             if items not in corpusDict:
@@ -90,8 +127,8 @@ def populaterCorpus(corpusBody=CORPUSBODY):
         for sentence in corpusDict.keys():
             linkedNodeList = n.LinkedList()
             splitSentence = sentence.split(" ")
-            for i in range(1, len(splitSentence)+1):
-                node = n.Node(splitSentence[i-1])
+            for i in range(1, len(splitSentence) + 1):
+                node = n.Node(splitSentence[i - 1])
                 linkedNodeList.insert(node)
                 if wordLinkedListDict.get(splitSentence[i - 1]):
                     wordLinkedListDict[splitSentence[i - 1]].append(linkedNodeList)
@@ -100,55 +137,54 @@ def populaterCorpus(corpusBody=CORPUSBODY):
                     linkedNodeListSequence = linkedNodeList.printBackwards(thisIsaList)
                     tempList = []
                     for item in linkedNodeListSequence:
-                      item.printLL()  
-                      tempList.append(item)
-                    wordLinkedListDict[splitSentence[i-1]] = tempList
+                        item.printLL()
+                        tempList.append(item)
+                    wordLinkedListDict[splitSentence[i - 1]] = tempList
 
 
 #              if '' in node.data:
 #                  continue
-     #           if (linkedNodeList) not in ngram:
-    #              ngram[linkedNodeList] = []
-   #               thisIsaList = []
-  #                linkedNodeListSequence = linkedNodeList.printBackwards(thisIsaList)
- #                 for item in linkedNodeListSequence:
+#           if (linkedNodeList) not in ngram:
+#              ngram[linkedNodeList] = []
+#               thisIsaList = []
+#                linkedNodeListSequence = linkedNodeList.printBackwards(thisIsaList)
+#                 for item in linkedNodeListSequence:
 #                    ngram[item].append(splitSentence[i-1])
 
-#taking out for a bit
+# taking out for a bit
 def getrResponse(message):
     out = ""
     outMinusIDK = ""
-    #processedMessage is a list of the message that was sent in a discord chat.
+    # processedMessage is a list of the message that was sent in a discord chat.
     processedMessage = processMessage(message)
     if int(len(processedMessage)) > 1:
-        #thisManyLengths = how many responses babyBot should gives
+        # thisManyLengths = how many responses babyBot should gives
         thisManyLengths = int(len(processedMessage) / 2)
     else:
         thisManyLengths = 1
     for i in range(thisManyLengths):
-      linkedListWord = random.choice(processedMessage)
-      if wordLinkedListDict.get(linkedListWord):
-            #this shouldn't be random eventually. Eventually we want the bot to decide which context is best using tags
-        outMinusIDK += random.choice(wordLinkedListDict[linkedListWord]).getLL()
-        out += random.choice(wordLinkedListDict[linkedListWord]).getLL() + "??????"
-      else:
-        #with the changes I have made in the morning of 11.15 this will no longer be hit. ProcessMessage now saves the response to ngram list so this logic will never hit false
-        out += " I do not know what {} means ".format(linkedListWord)
+        linkedListWord = random.choice(processedMessage)
+        if wordLinkedListDict.get(linkedListWord):
+            # this shouldn't be random eventually. Eventually we want the bot to decide which context is best using tags
+            outMinusIDK += random.choice(wordLinkedListDict[linkedListWord]).getLL()
+            out += random.choice(wordLinkedListDict[linkedListWord]).getLL() + "??????"
+        else:
+            # with the changes I have made in the morning of 11.15 this will no longer be hit. ProcessMessage now saves the response to ngram list so this logic will never hit false
+            out += " I do not know what {} means ".format(linkedListWord)
 
-        #while True:
-        #   if linkedList not in ngram.keys():
-        #      break
-        # third = random.choice(list(ngram[linkedList]))
-        #if ngram.get(third):
-        # out += ngram.get(third).getLL() + ' '
+            # while True:
+            #   if linkedList not in ngram.keys():
+            #      break
+            # third = random.choice(list(ngram[linkedList]))
+            # if ngram.get(third):
+            # out += ngram.get(third).getLL() + ' '
 
-
-#        word_pair = (third[1], third)
+    #        word_pair = (third[1], third)
 
     f = open("newCorpus/interactions.txt", "a")
-    f.write(message + " " + "\n" + outMinusIDK + "\n")
+    f.write(message + " " + "\n" + out + "\n")
     f.close()
-    
+
     return out
 
 
@@ -160,37 +196,36 @@ def processrMessage(message):
     messageList = list()
     messageList = message.split(" ")
     splitSentence = message.split(' ')
-    for i in range(1, len(splitSentence)+1):
-      linkedNodeList = n.LinkedList()
-      node = n.Node(splitSentence[i-1])
-      linkedNodeList.insert(node)
-      if wordLinkedListDict.get(splitSentence[i - 1]):
-        wordLinkedListDict[splitSentence[i - 1]].append(linkedNodeList)
-      else:
-        thisIsaList = []
-        linkedNodeListSequence = linkedNodeList.printBackwards(thisIsaList)
-        tempList = []
-        for item in linkedNodeListSequence:
-          additionalTempList = []
-          additionalTempList.append(item)
-          print(item.getLL())
-          tempList.append(item)
-          if wordLinkedListDict.get(splitSentence[i - 1]):
-            wordLinkedListDict[splitSentence[i-1]] = wordLinkedListDict.get(splitSentence[i-1]).append(item)
-          else:
-            wordLinkedListDict[splitSentence[i-1]] = tempList
-            wordLinkedListDict[splitSentence[i-1]] = additionalTempList
+    for i in range(1, len(splitSentence) + 1):
+        linkedNodeList = n.LinkedList()
+        node = n.Node(splitSentence[i - 1])
+        linkedNodeList.insert(node)
+        if wordLinkedListDict.get(splitSentence[i - 1]):
+            wordLinkedListDict[splitSentence[i - 1]].append(linkedNodeList)
+        else:
+            thisIsaList = []
+            linkedNodeListSequence = linkedNodeList.printBackwards(thisIsaList)
+            tempList = []
+            for item in linkedNodeListSequence:
+                additionalTempList = []
+                additionalTempList.append(item)
+                print(item.getLL())
+                tempList.append(item)
+                if wordLinkedListDict.get(splitSentence[i - 1]):
+                    wordLinkedListDict[splitSentence[i - 1]] = wordLinkedListDict.get(splitSentence[i - 1]).append(item)
+                else:
+                    wordLinkedListDict[splitSentence[i - 1]] = tempList
+                    wordLinkedListDict[splitSentence[i - 1]] = additionalTempList
     #      tempList = list()
-   #       tempList.append(linkedNodeList)
-     #     wordLinkedListDict[splitSentence[i - 1]] = tempList
+    #       tempList.append(linkedNodeList)
+    #     wordLinkedListDict[splitSentence[i - 1]] = tempList
 
-
-#              if '' in node.data:
-#                  continue
-     #   if (linkedNodeList) not in ngram:
+    #              if '' in node.data:
+    #                  continue
+    #   if (linkedNodeList) not in ngram:
     #      ngram[linkedNodeList] = []
-   #       thisIsaList = []
-  #        linkedNodeListSequence = linkedNodeList.printBackwards(thisIsaList)
- #         for item in linkedNodeListSequence:
-#            ngram[item].append(splitSentence[i-1])
+    #       thisIsaList = []
+    #        linkedNodeListSequence = linkedNodeList.printBackwards(thisIsaList)
+    #         for item in linkedNodeListSequence:
+    #            ngram[item].append(splitSentence[i-1])
     return messageList
